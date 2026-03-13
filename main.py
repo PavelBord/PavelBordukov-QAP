7ff53a0c4886ce1c91420f8dae5e194d4a9aa084
Pavel Bordukov <pawwel611@mail.ru>
2026-03-13 13:29:19 +0300

    Second

--- main.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введите число: "))
-seen = set()
-while n != 1 and n not in seen:
-    seen.add(n)
-n = sum(int(d) ** 2 for d in str(n))
-if n == 1:
-    print("True")
-else:
-    print("False")
